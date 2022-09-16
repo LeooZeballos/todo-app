dc3d19a6d41284e7db50d5369c05c7c1086ef559
LeooZeballos <48022327+LeooZeballos@users.noreply.github.com>
2022-09-16 18:57:29 +0000

    search bar without icon

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,7 +3,12 @@ import './TodoSearch.css';
 
 function TodoSearch(){
     return(
-        <input placeholder="Cebolla" />
+        <div className="input-group rounded">
+            <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+            <span class="input-group-text border-0" id="search-addon">
+                <i class="fas fa-search"></i>
+            </span>
+        </div>
     );
 }
 
