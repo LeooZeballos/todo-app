91862ef010ec253b9d4f7647f538e3d5adde0f50
LeooZeballos <48022327+LeooZeballos@users.noreply.github.com>
2022-12-28 23:22:53 +0000

    commentsssss

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { AppUI } from "./AppUI";
 
+// Default todos
 const defaultTodos = [
   { text: "Estudiar React", completed: false },
   { text: "Comer", completed: false },
@@ -10,35 +11,54 @@ const defaultTodos = [
 ];
 
 function App() {
+  // Use State Hook to manage the state of the todos
   const [todos, setTodos] = React.useState(defaultTodos);
+  // Use State Hook to manage the state of the search input
   const [searchValue, setSearchValue] = React.useState("");
+  // Number of completed todos
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
 
+  // Function to complete a todo
   const completeTodo = (text) => {
+    // Find the index of the todo to complete by its text
     const todoIndex = todos.findIndex((todo) => todo.text === text);
+    // Create a copy of the todos array
     const newTodos = [...todos];
+    // Set the completed property of the todo to true
     newTodos[todoIndex].completed = true;
+    // Update the todos state
     setTodos(newTodos);
   };
 
+  // Function to delete a todo
   const deleteTodo = (text) => {
+    // Find the index of the todo to delete by its text
     const todoIndex = todos.findIndex((todo) => todo.text === text);
+    // Create a copy of the todos array
     const newTodos = [...todos];
+    // Remove the todo from the array
     newTodos.splice(todoIndex, 1);
+    // Update the todos state
     setTodos(newTodos);
   };
 
+  // Filter the todos by the search value
   let searchedTodos = [];
   if (!searchValue.length >= 1) {
+    // If the search value is empty, return all the todos
     searchedTodos = todos;
   } else {
+    // If the search value is not empty, return the todos that match the search value
     searchedTodos = todos.filter((todo) => {
+      // Convert the todo text and the search value to lowercase
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
+      // Return true if the todo text includes the search value
       return todoText.includes(searchText);
     });
   }
 
+  // Render the AppUI component
   return (
     <AppUI
       todos={todos}
